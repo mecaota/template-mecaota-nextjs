b123e6b34a3efeb4155c5e06f5e859ede16736fc
mecaota <24618982+mecaota@users.noreply.github.com>
2026-04-23 18:19:21 +0900

    chore: 開発・ビルド用としてのviteを削除

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from '@vitejs/plugin-react';
-import wyw from '@wyw-in-js/vite';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), wyw()],
-});
